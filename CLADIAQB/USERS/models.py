de334ae81fef48c3bbb45ae2f5290802ce4105d3
taghawilfred <taghawilfred@gmail.com>
2024-09-04 14:44:54 +0100

    added organization model

--- CLADIAQB/USERS/models.py
+++ CLADIAQB/USERS/models.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class Organization(models.Model):
+    name = models.CharField(max_length=25)
+    created_at = models.Date.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
 class CustomUser(AbstractUser):
     # Add any additional fields you want here
     address = models.TextField(blank=True, null=True)
@@ -11,3 +18,16 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+class UserOrganizationRole(models.Model):
+    USER_TYPE_CHOICES = (
+        ('admin', 'Admin'),
+        ('org_user', 'Organization User')
+    )
+    
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
+
+    class Meta:
+        unique_together = ('user', 'organization')
